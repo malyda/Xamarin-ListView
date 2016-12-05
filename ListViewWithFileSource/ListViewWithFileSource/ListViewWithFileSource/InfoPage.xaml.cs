dd85f6543954d43618a55dea42310cff30259a88
Malý David <maly.david.it@gmail.com>
2016-12-05 19:50:06 +0100

    ListView and Second Page support
    Editted:
    - FormattedListView
    - NotFormattedListView
    - InfoPage created

---
+++ ListViewWithFileSource/ListViewWithFileSource/ListViewWithFileSource/InfoPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ListViewWithFileSource
+{
+    public partial class InfoPage : ContentPage
+    {
+        public InfoPage()
+        {
+            InitializeComponent();
+        }
+
+        public InfoPage(Person person)
+        {
+            InitializeComponent();
+            NameLabel.Text = person.Surname;
+            SurnameLabel.Text = person.Lastname;
+            AgeLabel.Text = person.Age.ToString();
+        }
+    }
+}
---
+++ ListViewWithFileSource/ListViewWithFileSource/ListViewWithFileSource/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewWithFileSource
+{
+   public class Person
+    {
+        public string Lastname { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public ImageSource ProfilePhoto { get; set; }
+
+        public string GetName => Lastname + " " + Surname;
+
+        public override string ToString()
+        {
+            return "Surname: " + Surname + " Lastname " + Lastname + " Age: "+ Age;
+        }
+    }
+}
